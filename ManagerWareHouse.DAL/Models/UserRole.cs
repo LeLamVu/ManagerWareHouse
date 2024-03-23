a33767c85224fc2d41f04fd7ee3589786ebf2dc4
LeLamVu <llvu_21pm@student.agu.edu.vn>
2024-03-23 16:39:47 +0700

    .DAL .create Add UserRole Model

---
+++ ManagerWareHouse.DAL/Models/UserRole.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerWareHouse.DAL.Models
+{
+    [Table("UserRole")]
+    public class UserRole
+    {
+        public int IDUserRole { get; set; }
+
+        public int IDUser { get; set; }
+        public virtual User User { get; set; }
+
+        public int IDRole { get; set; }
+        public virtual Role Role { get; set; }
+    }
+}
