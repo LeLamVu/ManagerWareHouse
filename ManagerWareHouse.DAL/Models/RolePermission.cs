33b80990d7dab5ad1cd45241088287fb8dc7a0a9
LeLamVu <llvu_21pm@student.agu.edu.vn>
2024-03-23 16:41:48 +0700

    .DAL .create Add RolePermission Model

---
+++ ManagerWareHouse.DAL/Models/RolePermission.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerWareHouse.DAL.Models
+{
+    [Table("RolePermission")]
+    public class RolePermission
+    {
+        public int IDRolePermission { get; set; }
+        
+        public int IDRole { get; set; }
+        public virtual Role Role { get; set; }
+
+        public int IDPermission { get; set; }
+        public virtual Permission Permission { get; set; }
+    }
+}
