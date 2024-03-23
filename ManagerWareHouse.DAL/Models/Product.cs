4a9f237ba055634640c982b445bb8e620dd7dc43
LeLamVu <llvu_21pm@student.agu.edu.vn>
2024-03-23 16:27:34 +0700

    .DAL .create Add Product Model

---
+++ ManagerWareHouse.DAL/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManagerWareHouse.DAL.Reponsitories;
+
+namespace ManagerWareHouse.DAL.Models
+{
+    [Table("Products")]
+    public class Product : ModelConstruct
+    {
+        public int IDProduct { get; set; }
+        public string CodeProduct { get; set; }
+        public string NameProduct { get; set; }
+        public string DescriptionProduct { get; set; }
+        public string Avatar { get; set; } = String.Empty;
+        public int? ProductPrice { get; set; } = 0;
+        public int? ProductQuanlity { get; set; } = 0;
+        public DateTime ProductDateOfManufacture { get; set; }
+        public DateTime ProductExpirationDate { get; set; }
+
+        public int IDCategory { get; set; }
+        public int IDSupplier { get; set; }
+        public int IDUnit { get; set; }
+        public int IDWareHouse { get; set; }
+
+        public virtual Category Category { get; set; }
+        public virtual Supplier Supplier { get; set; }
+        public virtual Unit Unit { get; set; }
+        public virtual WareHouse WareHouse { get; set; }
+
+    }
+}
