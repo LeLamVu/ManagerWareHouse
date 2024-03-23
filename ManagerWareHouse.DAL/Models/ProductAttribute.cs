d2d04015053a56a20b32231baf00a78d8208fcc0
LeLamVu <llvu_21pm@student.agu.edu.vn>
2024-03-23 16:30:10 +0700

    .DAL .create Add ProductAttribute Model

---
+++ ManagerWareHouse.DAL/Models/ProductAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerWareHouse.DAL.Models
+{
+    [Table("ProductAttributes")]
+    public class ProductAttribute
+    {
+        public int IDProductAttribute { get; set; }
+        public int IDProduct { get; set; }
+        public int IDAttribute { get; set; }
+        public string ValueAttribute { get; set; }
+        public string URLAttribute { get; set; } = String.Empty;
+    }
+}
