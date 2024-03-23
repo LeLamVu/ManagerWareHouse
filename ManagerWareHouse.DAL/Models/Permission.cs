4cda41f95ab16c3128600388e4d0a956875adc77
LeLamVu <llvu_21pm@student.agu.edu.vn>
2024-03-23 16:37:52 +0700

    .DAL .create Add Permission Model

---
+++ ManagerWareHouse.DAL/Models/Permission.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerWareHouse.DAL.Models
+{
+    [Table("Permission")]
+    public class Permission
+    {
+        public int IDPermission { get; set; }
+        public string NamePermission { get; set; }
+    }
+}
