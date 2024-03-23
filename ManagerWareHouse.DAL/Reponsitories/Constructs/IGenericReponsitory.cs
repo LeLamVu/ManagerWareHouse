d1f23e45a6e478c32658dd24c38840a89a26a2e9
LeLamVu <llvu_21pm@student.agu.edu.vn>
2024-03-23 16:18:16 +0700

    .DAL .create Add Generic Reponsitory Interface

---
+++ ManagerWareHouse.DAL/Reponsitories/Constructs/IGenericReponsitory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerWareHouse.DAL.Reponsitories.Constracts
+{
+    public interface IGenericReponsitory<T> where T: class
+    {
+        void Create(T _object);
+        void Update(T _object);
+        void Delete(T _object);
+        Task<IEnumerable<T>> GetAll();
+        T GetByID(int id);
+        void SaveToDatabase();
+    }
+}
