9f7ecc8710e4e212748496228efc02fe84f428ad
LeLamVu <llvu_21pm@student.agu.edu.vn>
2024-03-23 16:21:50 +0700

    .DAL .create Add Model Construct Class

---
+++ ManagerWareHouse.DAL/Reponsitories/ModelConstruct.cs
@@ -0,0 +1,15 @@
+﻿using ManagerWareHouse.DAL.Reponsitories.Constracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerWareHouse.DAL.Reponsitories
+{
+    public class ModelConstruct : IModelConstruct
+    {
+        public DateTime CreateAt { get; set; }
+        public DateTime? UpdateAt { get; set; }
+    }
+}
