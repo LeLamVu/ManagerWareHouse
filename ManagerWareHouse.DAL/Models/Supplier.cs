1b21563fd3034dd4cda325ca71730c30a69d4b7e
LeLamVu <llvu_21pm@student.agu.edu.vn>
2024-03-23 16:26:50 +0700

    .DAL .create Add Supplier Model

---
+++ ManagerWareHouse.DAL/Models/Supplier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManagerWareHouse.DAL.Reponsitories;
+
+namespace ManagerWareHouse.DAL.Models
+{
+    [Table("Supplier")]
+    public class Supplier : ModelConstruct
+    {
+        public int IDSupplier { get; set; }
+        public string NameSupplier { get; set; }
+        public string AddressSupplier { get; set; }
+        public string PhoneSupplier { get; set; }
+        public string ImageSupplier { get; set; } = String.Empty;
+
+        public virtual IEnumerable<Product> Products { get; set; }
+    }
+}
