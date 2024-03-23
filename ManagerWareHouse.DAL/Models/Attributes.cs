fa5de38ca3587b64137e728e65ef0078fab99e8c
LeLamVu <llvu_21pm@student.agu.edu.vn>
2024-03-23 16:29:26 +0700

    .DAL .create Add Attribute Model

---
+++ ManagerWareHouse.DAL/Models/Attributes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManagerWareHouse.DAL.Reponsitories;
+
+namespace ManagerWareHouse.DAL.Models
+{
+    [Table("Attributes")]
+    public class Attributes : ModelConstruct
+    {
+        public int IDAttribute { get; set; }
+        public string NameAttribute { get; set; }
+        public string DescriptionAttribute { get; set; } = String.Empty;
+    }
+}
