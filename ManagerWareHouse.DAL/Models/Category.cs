0939fac57b2661bfaf22858feaa25545494b1502
LeLamVu <llvu_21pm@student.agu.edu.vn>
2024-03-23 16:26:01 +0700

    .DAL .create Add Category Model

---
+++ ManagerWareHouse.DAL/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using ManagerWareHouse.DAL.Reponsitories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerWareHouse.DAL.Models
+{
+    [Table("Categories")]
+    public class Category : ModelConstruct
+    {
+        public int IDCategory { get; set; }
+        public string NameCategory { get; set; }
+        public string DescriptionCategory { get; set; } = String.Empty;
+
+        public virtual IEnumerable<Product> Products { get; set; }
+    }
+}
