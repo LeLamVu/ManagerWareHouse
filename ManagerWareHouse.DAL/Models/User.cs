30c3ce2267aadff266537012e5ac8c1f67712dd9
LeLamVu <llvu_21pm@student.agu.edu.vn>
2024-03-23 16:31:11 +0700

    .DAL .create Add User Model

---
+++ ManagerWareHouse.DAL/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerWareHouse.DAL.Models
+{
+    [Table("User")]
+    public class User
+    {
+        public int IDUser { get; set; }
+        public string NameUser { get; set; }
+        public string AddressUser { get; set; }
+        public DateTime? BirthdayUser { get; set; }
+        public string PhoneUser { get; set; }
+        public string Avatar { get; set; } = String.Empty;
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public int? IDWareHouse { get; set; }
+        public int IDRole { get; set; }
+
+        public virtual WareHouse WareHouse { get; set; } = null;
+        public virtual Role Role { get; set; }
+        
+    }
+}
