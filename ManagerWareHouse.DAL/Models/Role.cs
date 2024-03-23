eebd6f6e2b62af38d4d29cffef9c03c718f4ccd0
LeLamVu <llvu_21pm@student.agu.edu.vn>
2024-03-23 16:31:48 +0700

    .DAL .create Add Role Model

---
+++ ManagerWareHouse.DAL/Models/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerWareHouse.DAL.Models
+{
+    [Table("Role")]
+    public class Role
+    {
+        public int IDRole { get; set; }
+        public string NameRole { get; set; }
+    }
+}
