618dfda4b37ee072ec887c59da450a7de19d7d26
LeLamVu <llvu_21pm@student.agu.edu.vn>
2024-03-23 16:25:18 +0700

    .DAL .create Add WareHouse Model

---
+++ ManagerWareHouse.DAL/Models/WareHouse.cs
@@ -0,0 +1,23 @@
+﻿using ManagerWareHouse.DAL.Reponsitories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerWareHouse.DAL.Models
+{
+    [Table("WareHouses")]
+    public class WareHouse: ModelConstruct
+    {
+        public int IDWareHouse { get; set; }
+        public string NameWareHouse { get; set; }
+        public string AdressWareHouse { get; set; }
+        public string PhoneWareHouse { get; set; }
+        public string DescriptionWareHouse { get; set; } = String.Empty;
+
+        public virtual IEnumerable<Product> Products { get; set; }
+    }
+}
