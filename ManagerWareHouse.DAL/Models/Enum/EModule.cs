1ee966273d5dbe4ab1ffdd6477af19b7461cbfd0
LeLamVu <llvu_21pm@student.agu.edu.vn>
2024-03-23 16:48:39 +0700

    .DAL .create Add EModule Enum

---
+++ ManagerWareHouse.DAL/Models/Enum/EModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerWareHouse.DAL.Models.Enum
+{
+    // module is system
+    public enum EModule
+    {
+        Warehouse,
+        Category,
+        Unit,
+        Supplier,
+        Product,
+        Attribute,
+        ProductAttribute,
+        User
+    }
+}
