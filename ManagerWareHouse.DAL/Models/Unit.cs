207dcbf9637ec4140b178ac8c76c972cedc79b19
LeLamVu <llvu_21pm@student.agu.edu.vn>
2024-03-23 16:28:37 +0700

    .DAL .create Add Unit Model

---
+++ ManagerWareHouse.DAL/Models/Unit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManagerWareHouse.DAL.Reponsitories;
+
+namespace ManagerWareHouse.DAL.Models
+{
+    [Table("Unit")]
+    public class Unit: ModelConstruct
+    {
+        public int IDUnit { get; set; }
+        public string NameUnit { get; set; }
+        public string DescriptionUnit { get; set; } = String.Empty;
+
+        public virtual IEnumerable<Product> Products { get; set; }
+    }
+}
