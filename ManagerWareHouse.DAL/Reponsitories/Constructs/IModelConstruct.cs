5d24becce28632f55d26b8b71095df4c712786a3
LeLamVu <llvu_21pm@student.agu.edu.vn>
2024-03-23 16:19:54 +0700

    .DAL .create Add Model Construct Interface

---
+++ ManagerWareHouse.DAL/Reponsitories/Constructs/IModelConstruct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerWareHouse.DAL.Reponsitories.Constracts
+{
+    public interface IModelConstruct
+    {
+        DateTime CreateAt { get; set; }
+        DateTime? UpdateAt { get; set; }
+    }
+}
